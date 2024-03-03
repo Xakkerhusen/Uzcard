af7a4cd8b94704cedf6c72f736e50d1436d0d8b5
Sirojiddin <orinboyevsirojiddin@gmail.com>
2024-03-03 22:05:15 +0500

    The company's update method has been completed

--- src/main/java/com/example/Uzcard/controller/CompanyController.java
+++ src/main/java/com/example/Uzcard/controller/CompanyController.java
@@ -1,11 +1,12 @@
 package com.example.Uzcard.controller;
 
-import com.example.Uzcard.dto.CompanyDTO;
 import com.example.Uzcard.service.CompanyService;
+import com.example.Uzcard.dto.CompanyDTO;
+import com.example.Uzcard.dto.UpdateCompanyDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,9 +17,16 @@ public class CompanyController {
 
     @PostMapping("/create")
 //    @PreAuthorize("hasRole('ADMIN')")
-    @Operation(summary = "API create company",description = "It is through the API that the company is created.Only ADMIN can apply to this API.")
+    @Operation(summary = "API create company", description = "It is through the API that the company is created.Only ADMIN can apply to this API.")
     public ResponseEntity<?> create(@RequestBody CompanyDTO dto) {
         return ResponseEntity.ok(companyService.create(dto));
+    }
+
+    @PutMapping("/update/{id}")
+//    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "API update company", description = "It is through the API that the company is update.Only ADMIN can apply to this API.")
+    public ResponseEntity<?> update(@PathVariable("id") String id,@Valid @RequestBody UpdateCompanyDTO dto) {
+        return ResponseEntity.ok(companyService.update(id, dto));
     }
 
 }
--- src/main/java/com/example/Uzcard/service/CompanyService.java
+++ src/main/java/com/example/Uzcard/service/CompanyService.java
@@ -1,16 +1,18 @@
 package com.example.Uzcard.service;
 
 import com.example.Uzcard.dto.CompanyDTO;
+import com.example.Uzcard.dto.UpdateCompanyDTO;
 import com.example.Uzcard.entity.CompanyEntity;
+import com.example.Uzcard.enums.AppLanguage;
 import com.example.Uzcard.enums.CompanyRole;
 import com.example.Uzcard.exp.AppBadException;
 import com.example.Uzcard.repository.CompanyRepository;
-import com.example.Uzcard.utils.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,8 +21,12 @@ public class CompanyService {
     @Autowired
     private CompanyRepository companyRepository;
 
+
+    @Autowired
+    private ResourceBundleService service;
+
     /**
-     *This method will create a company through which the phone number must be no job.
+     * This method will create a company through which the phone number must be no job.
      * Again the company code should be sent to beriberi on the front side only if the role is banking
      * The role is created randomly if there is no BANK.
      */
@@ -41,6 +47,25 @@ public class CompanyService {
         entity.setPhoneNummber(dto.getPhoneNummber());
         companyRepository.save(entity);
         return "create successful";
+    }
+
+    public String update(String id, UpdateCompanyDTO dto) {
+        Optional<CompanyEntity> optional = companyRepository.findById(id);
+        if (optional.isEmpty()) {
+            String message = service.getMessage("company.not.fount", AppLanguage.UZ);
+            log.warn(message);
+            throw new AppBadException(message);
+        }
+        CompanyEntity entity = optional.get();
+        entity.setAddress(dto.getAddress());
+        entity.setName(dto.getName());
+        entity.setPassword(dto.getPassword());
+        entity.setPhoneNummber(dto.getPhoneNumber());
+        entity.setCode(dto.getCode());
+        entity.setContact(dto.getContact());
+        companyRepository.save(entity);
+        return "update successful";
+
     }
 
 }
